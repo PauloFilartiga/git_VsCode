41568950dfdbbb7541ee91458384749b561801d3
filartiga <tikusoftware@outlook.com>
2023-09-01 16:19:48 -0400

    Atualizado 2.0

--- caso_empresa.postgres.sql
+++ caso_empresa.postgres.sql
@@ -11,7 +11,7 @@ CREATE TABLE divisao (
 );
 
 CREATE TABLE empregado (
-    matri INTEGER NOT NULL,
+    matr INTEGER NOT NULL,
     nome VARCHAR(50),
     data_lotacao TIMESTAMP,
     lotacao INTEGER,
@@ -20,4 +20,33 @@ CREATE TABLE empregado (
     gerencia_div INTEGER NOT NULL
 );
 
+CREATE TABLE dependente (
+    matr INTEGER NOT NULL PRIMARY FOREIGN KEY,
+    nome VARCHAR(50) NOT NULL,
+    endereco VARCHAR(50)
+);
+
+CREATE TABLE emp_desc (
+    cod_desc INTEGER NOT NULL PRIMARY FOREIGN KEY,
+    matr INTEGER NOT NULL PRIMARY FOREIGN KEY
+);
+
+CREATE TABLE desconto (
+    cod_desc INTEGER NOT NULL,
+    nome VARCHAR(50),
+    tipo VARCHAR(10),
+    valor DECIMAL(10)
+);
+
+CREATE TABLE emp_venc (
+    cod_venc INTEGER NOT NULL,
+    matr INTEGER NOT NULL PRIMARY FOREIGN KEY
+);
+
+CREATE TABLE vencimento (
+    cod_venc INTEGER NOT NULL,
+    nome VARCHAR(50) ALTERNATIVE KEY,
+    tipo VARCHAR(10),
+    valor DECIMAL(10)
+);
 
